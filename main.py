e790d6891e2e1d600e33ac8db7abf3b3fabe7a61
Cenk Gun <cenk.gun@hepsiburada.com>
2025-01-14 15:46:30 +0300

    Change files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 app = Flask(__name__)
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("harita-1195-040ed4087bc0.json", scope)
 client = gspread.authorize(creds)
 
 spreadsheet = client.open_by_key("1wYKfXFreMqSbRri0w275rLTvntKoXjWbDBmvyFbXEDk")
