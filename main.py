f23673926f5afe8a5d767c45f987bbde705a5d66
Cenk Gun <cenk.gun@hepsiburada.com>
2025-01-15 08:43:13 +0300

    Setup environment variables

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import os
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import base64
 
 app = Flask(__name__)
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds_json = os.getenv('GOOGLE_CREDENTIALS')
-creds_dict = json.loads(creds_json)
 
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-client = gspread.authorize(creds)
-
-spreadsheet = client.open_by_key("1wYKfXFreMqSbRri0w275rLTvntKoXjWbDBmvyFbXEDk")
-sheet = spreadsheet.sheet1
+try:
+    creds_base64 = os.getenv('GOOGLE_CREDENTIALS')
+    if creds_base64:
+        creds_json = base64.b64decode(creds_base64).decode('utf-8')
+        creds_dict = json.loads(creds_json)
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+        client = gspread.authorize(creds)
+        spreadsheet = client.open_by_key("1wYKfXFreMqSbRri0w275rLTvntKoXjWbDBmvyFbXEDk")
+        sheet = spreadsheet.sheet1
+except Exception as e:
+    print(f"Error loading credentials: {str(e)}")
 
 
 @app.route('/add_data', methods=['POST'])
@@ -34,7 +39,6 @@ def add_data():
             data.get('pml', '')
         ]
         sheet.append_row(row)
-
         return jsonify({"message": "Successfully completed!"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
